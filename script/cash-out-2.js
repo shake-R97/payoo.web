5bac616d9b8c07b1e2608e7d66cb28fdb3f5c7ec
sheikhrabby97 <sheikhrabby97@gmail.com>
2025-04-10 19:25:16 +0600

    updated cashout section done

---
+++ script/cash-out-2.js
@@ -0,0 +1,16 @@
+document.getElementById('cash-btn')
+.addEventListener('click', function(event){
+    const pin = document.getElementById('cashout-pin').value;
+    const figures = document.getElementById('cash-out-figures').value;
+    const convertFigures = parseFloat(figures);
+    const balanceM = document.getElementById('balance-main').innerText;
+    const convertBalanceM = parseFloat(balanceM);
+
+    if(pin === '9797Z'){
+        const sum = convertBalanceM - convertFigures;
+        const newBalance = document.getElementById('balance-main').innerText =  sum;
+    }
+    else{
+        alert("Wrong Credential's or Password");
+    }
+})
